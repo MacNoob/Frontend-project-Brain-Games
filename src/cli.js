dfbaa122ae7f50827b362319264030a839ac484c
Andrey Tiutiunikov <mcmagus@mail.ru>
2022-09-14 15:23:29 +0300

    Update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,6 @@
+/* eslint-disable import/prefer-default-export */
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!')
+console.log('Welcome to the Brain Games!');
+
 export const userName = readlineSync.question('May I have your name? ');
