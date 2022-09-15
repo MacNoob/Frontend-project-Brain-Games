c48821f855be4ab1a14e7eb819427aca9bf42d18
Andrey Tiutiunikov <mcmagus@mail.ru>
2022-09-15 18:30:00 +0300

    add directory games/

---
+++ games/calc/calc.js
@@ -0,0 +1,5 @@
+const calcGame = () => {
+  const evenCondition = console.log('What is the result of the expression?');
+  return evenCondition;
+};
+export default calcGame;
---
+++ games/even/even.js
@@ -0,0 +1,10 @@
+const evenGame = () => {
+  const evenCondition = console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  return evenCondition;
+};
+
+export const greeting = () => {
+  console.log('Welcome to the brain games!');
+};
+
+export default evenGame;
