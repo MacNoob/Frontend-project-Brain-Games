7c362624529760ff2c5b492b7d8a4b2678a13104
Andrey Tiutiunikov <mcmagus@mail.ru>
2022-09-17 13:09:02 +0300

    Update games/even/even.js

--- games/even/even.js
+++ games/even/even.js
@@ -1,10 +1,29 @@
-const evenGame = () => {
+import {
+  random, answer, correctAnswer, opposite, errorMessage, congratulations,
+} from '../../src/index.js';
+
+// Условия игры:
+export const evenGameConditions = () => {
   const evenCondition = console.log('Answer "yes" if the number is even, otherwise answer "no".');
   return evenCondition;
 };
 
-export const greeting = () => {
-  console.log('Welcome to the brain games!');
-};
+// Движок:
+export const gameLogic = () => {
+  for (let i = 0; i < 3; i += 1) {
+    let pushNumber = 0;
+    const multNumber = 10;
+    pushNumber = random(multNumber);
+    console.log('Questions: ', pushNumber);
+    const pushAnswer = answer();
 
-export default evenGame;
+    const checking1 = (pushNumber % 2 === 0) && (pushAnswer === 'yes');
+    const checking2 = (pushNumber % 2 !== 0) && (pushAnswer === 'no');
+    if (checking1 === true || checking2 === true) {
+      correctAnswer();
+    } else if (pushAnswer !== ('yes' || 'no')) {
+      return opposite(errorMessage());
+    }
+  }
+  return congratulations();
+};
