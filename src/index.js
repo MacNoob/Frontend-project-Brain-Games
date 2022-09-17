a4bd0e043d2fa637777b2a3b1d236aed18937303
Andrey Tiutiunikov <mcmagus@mail.ru>
2022-09-17 13:09:34 +0300

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+// Знакомство;
+export const acquaintance = () => {
   console.log('Welcome to the brain games!');
 };
 
-export const acquaintance = () => {
-  const userName = readlineSync.question('May I have your name? ');
+// Переменная кот. будет хранить имя игрока;
+let userName;
+
+// Получение имени игрока;
+export const getName = () => {
+  userName = readlineSync.question('May I have your name? ');
+};
+
+// Приветствие игрока по имени;
+export const greeting = () => {
   console.log(`Hello, ${userName}`);
 };
 
-export function getRandomInt(min, max) {
-  const randomNumber = min + Math.floor(Math.random() * (max - min + 1));
+// Генерация рандомного числа от 0 до 10;
+export const random = (number) => {
+  const randomNumber = Math.floor(Math.random() * number);
   return randomNumber;
-}
-
-export const randomOperation = () => {
-  const subtraction = '-';
-  const addition = '+';
-  const multiplication = '*';
-  const randomIndex = Math.floor(Math.random() * 3);
-  const randomize = [subtraction, multiplication, addition];
-  return console.log(randomize(randomIndex));
 };
+
+// Получение ответа игрока;
+let userAnswer;
+export const answer = () => {
+  userAnswer = readlineSync.question('You answer: ');
+};
+
+// Функция для использования в случае корректного ответа:
+export const correctAnswer = () => {
+  console.log('Correct!');
+};
+
+// Проверка условия для even:
+export const opposite = () => (answer === 'yes' ? 'no' : 'yes');
+
+// Сообщение об ошибочном ответе:
+export const errorMessage = () => {
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${opposite()}'. \n Let's try again, ${userName}!`);
+};
+
+// Поздравление игрока:
+export const congratulations = () => console.log(`Congratulations, ${userName}!`);
