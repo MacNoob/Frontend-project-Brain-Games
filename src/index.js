7373d5262821f188870d9d64ed54637c65fc0560
Andrey Tiutiunikov <mcmagus@mail.ru>
2022-09-20 14:14:36 +0300

    index.js fix

--- src/index.js
+++ src/index.js
@@ -1,54 +1,28 @@
 import readlineSync from 'readline-sync';
 
-// Знакомство;
-export const acquaintance = () => {
+const gameEngine = (description, task) => {
   console.log('Welcome to the brain games!');
-};
-
-// Переменная кот. будет хранить имя игрока;
-let userName;
-
-// Получение имени игрока;
-export const getName = () => {
-  userName = readlineSync.question('May I have your name? ');
-};
-
-// Приветствие игрока по имени;
-export const greeting = () => {
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
+
+  console.log(description);
+
+  for (let i = 0; i < 3; i += 1) {
+    const [question, correctAnswer] = task();
+
+    console.log(`Question: ${question}`);
+
+    const answer = readlineSync.question('Your answer?: ');
+
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \n Let's try again, ${userName}!`);
+
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
-// Задаем вопрос:
-export const questions = (meaning) => console.log('Questions: ', meaning);
-
-// Генерация рандомного числа от 0 до 10;
-export const random = (number = 10) => {
-  const randomNumber = Math.floor(Math.random() * number);
-  return randomNumber;
-};
-
-// Получение ответа игрока;
-let userAnswer;
-export const answer = () => {
-  userAnswer = readlineSync.question('You answer: ');
-  return userAnswer;
-};
-
-// Функция для использования в случае корректного ответа:
-export const correctAnswer = () => {
-  console.log('Correct!');
-};
-
-// Проверка условия для even:
-export const opposite = () => {
-  const opposite1 = (userAnswer === 'yes' ? 'no' : 'yes');
-  return opposite1;
-};
-
-// Сообщение об ошибочном ответе:
-export const errorMessage = () => {
-  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${opposite()}'. \n Let's try again, ${userName}!`);
-};
-
-// Поздравление игрока
-export const congratulations = () => console.log(`Congratulations, ${userName}!`);
+export default gameEngine;
