be834aef304d2dd16f2351d4f12a3a92cffa13f3
Andrey Tiutiunikov <mcmagus@mail.ru>
2022-09-30 01:04:40 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greatest = () => {
-  console.log('Welcome to the brain games!');
+  console.log('Welcome to the Brain Games!');
   const getUserName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${getUserName}`);
 };
