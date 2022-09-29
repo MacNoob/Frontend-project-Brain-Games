d84da737ca51225e5aa06747db947473a1adda47
Andrey Tiutiunikov <mcmagus@mail.ru>
2022-09-29 20:12:44 +0300

    Create directory /progression

---
+++ games/progression/progression.js
@@ -0,0 +1,42 @@
+import gameEngine from '../../src/index.js';
+
+const description = ('What number is missing in the progression?');
+
+const makeProgression = () => {
+  const startNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+  let variableOfStartNumber = startNumber(2, 10);
+
+  const multiplier = (min, max) => Math.floor(Math.random() * (max - min) + min);
+  const variableOfMultiplier = multiplier(2, 5);
+
+  const quantity = (min, max) => Math.floor(Math.random() * (max - min) + min);
+  const variableOfQuantity = quantity(5, 10);
+
+  const arr = [];
+
+  for (let i = 0; i < variableOfQuantity; i += 1) {
+    arr.push(variableOfStartNumber);
+    variableOfStartNumber += variableOfMultiplier;
+  }
+
+  return arr;
+};
+const task = () => {
+  const progress = makeProgression();
+
+  const randomIndex = Math.floor(Math.random() * progress.length);
+
+  const correctAnswer = progress[randomIndex];
+
+  const temp = progress[randomIndex];
+  progress[randomIndex] = '..';
+
+  const question = progress;
+
+  const result = [question, String(correctAnswer)];
+  return result;
+};
+
+const gameProgressionStart = () => gameEngine(description, task);
+
+export default gameProgressionStart;
