ab1a901b5b53f14c8aebbd0f88ef9c0753f62ff5
Andrey Tiutiunikov <mcmagus@mail.ru>
2022-10-04 17:31:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 import readlineSync from 'readline-sync';
 
-const gameEngine = (description, task) => {
+const attempsCount = 3;
+
+const processTheTask = (description, getTask) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
 
   console.log(description);
 
-  const numbersOfAttemps = 3;
-
-  for (let i = 0; i < numbersOfAttemps; i += 1) {
-    const [question, correctAnswer] = task();
+  for (let i = 0; i < attempsCount; i += 1) {
+    const [question, correctAnswer] = getTask();
 
     console.log(`Question: ${question}`);
 
     const userAnswer = readlineSync.question('Your answer?: ');
 
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \n Let's try again, ${userName}!`);
+    switch (userAnswer) {
+      case correctAnswer:
+        console.log('Correct!');
+        break;
+      default: console.log(`'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'.`);
+        console.log(`Let's try again, ${userName}!`);
 
-      return;
+        return;
     }
   }
   console.log(`Congratulations, ${userName}!`);
 };
 
-export default gameEngine;
+export default processTheTask;
