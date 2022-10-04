c8a9c5be78ed3f48525992e8e6f40efee91b90fb
Andrey Tiutiunikov <mcmagus@mail.ru>
2022-10-04 17:23:49 +0300

    Update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import gameProgressionStart from '../games/progression/progression.js';
+import startProgression from '../games/progression.js';
 
-gameProgressionStart();
+startProgression();
