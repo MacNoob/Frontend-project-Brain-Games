2fb58b2f9a1d4085812d90b0e065db684c0bc848
Andrey Tiutiunikov <mcmagus@mail.ru>
2022-10-06 18:34:18 +0300

    Update calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,25 +2,23 @@ import getRandomNumber from '../utils.js';
 
 export const description = ('What is the result of the expression?');
 
-let correctAnswer;
-let question;
+const operators = ['+', '-', '*'];
+
 export const calculate = () => {
   const randomNumberOne = getRandomNumber(1, 100);
   const randomNumberTwo = getRandomNumber(1, 100);
-  const operators = ['+', '-', '*'];
-  const operator = Math.floor(Math.random() * operators.length);
-  switch (operator) {
-    case operators.indexOf('+'):
+  const randomOperator = operators[getRandomNumber(0, 2)];
+  const question = `${randomNumberOne} ${randomOperator} ${randomNumberTwo}`;
+  let correctAnswer;
+  switch (randomOperator) {
+    case '+':
       correctAnswer = randomNumberOne + randomNumberTwo;
-      question = `${randomNumberOne} ${operators[0]} ${randomNumberTwo}`;
       break;
-    case operators.indexOf('-'):
+    case '-':
       correctAnswer = randomNumberOne - randomNumberTwo;
-      question = `${randomNumberOne} ${operators[1]} ${randomNumberTwo}`;
       break;
-    case operators.indexOf('*'):
+    case '*':
       correctAnswer = randomNumberOne * randomNumberTwo;
-      question = `${randomNumberOne} ${operators[2]} ${randomNumberTwo}`;
       break;
     default:
       break;
